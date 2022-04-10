5630e168b02ec0b340ae62ca1f6b76c5a04bd60b
Mohamed Maged <mohamedmaged.live@gmail.com>
2022-04-10 23:04:06 +0200

    create API models

---
+++ MATICTask/Models/Model.swift
@@ -0,0 +1,37 @@
+//
+//  Model.swift
+//  MATICTask
+//
+//  Created by Mohamed Maged on 10/04/2022.
+
+import Foundation
+
+// MARK: - RepositoryResponse
+struct RepositoryResponse: Codable {
+    let totalCount: Int
+    let incompleteResults: Bool
+    let items: [Repository]
+}
+
+// MARK: - Repository
+struct Repository: Codable {
+    let id: Int
+    let name: String
+    let description: String?
+    let stargazersCount: Int
+    let openIssuesCount: Int
+    let createdAt, updatedAt: Date
+}
+
+// MARK: - Owner
+struct Owner: Codable {
+    let login: String
+    let id: Int
+    let avatarURL: String
+   
+
+    enum CodingKeys: String, CodingKey {
+        case login, id
+        case avatarURL = "avatar_url"
+    }
+}
---
+++ MATICTask/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MATICTask
+//
+//  Created by Mohamed Maged on 10/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+   }
+}
